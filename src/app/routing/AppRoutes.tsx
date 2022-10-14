13048831d139c63e57e687f442dae9745caf7cea
infinite <infinite@ib-synergy.co.id>
2022-10-14 13:46:20 +0700

    m

--- src/app/routing/AppRoutes.tsx
+++ src/app/routing/AppRoutes.tsx
@@ -11,6 +11,7 @@ import {PrivateRoutes} from './PrivateRoutes'
 import {ErrorsPage} from '../modules/errors/ErrorsPage'
 import {Logout, AuthPage, useAuth} from '../modules/auth'
 import {App} from '../App'
+import { MasterLayout } from '../../_metronic/layout/MasterLayout'
 
 /**
  * Base URL of the website.
@@ -20,24 +21,24 @@ import {App} from '../App'
 const {PUBLIC_URL} = process.env
 
 const AppRoutes: FC = () => {
-  // const {currentUser} = useAuth()
+  const {currentUser} = useAuth()
   return (
     <BrowserRouter basename={PUBLIC_URL}>
       <Routes>
         <Route element={<App />}>
           <Route path='error/*' element={<ErrorsPage />} />
           <Route path='logout' element={<Logout />} />
-          {/* {currentUser ? (
+          {currentUser ? (
             <>
               <Route path='/*' element={<PrivateRoutes />} />
               <Route index element={<Navigate to='/inputData' />} />
             </>
           ) : (
-            <> */}
+            <>
           <Route path='/*' element={<PrivateRoutes />} />
           <Route index element={<Navigate to='/inputData' />} />
-          {/* </>
-          )} */}
+          </>
+          )}
         </Route>
       </Routes>
     </BrowserRouter>
